fc1a3ff8cfe5ffe2ba8b6432a42d2706ef668f21
oguzcc <oguzcc@gmail.com>
2022-02-13 20:45:00 +0300

    auth bugs fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,18 @@
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const { Patient } = require('../models/patient');
 const { Doctor } = require('../models/doctor');
+const { Patient } = require('../models/patient');
 const express = require('express');
 const router = express.Router();
 
 // Auth a person
 router.post('/', async (req, res) => {
-  const doctor = await Doctor.findOne({ email: req.body.email }).select(
-    '-password -__v'
-  );
-  const patient = await Patient.findOne({ email: req.body.email }).select(
-    '-password -__v'
-  );
-  let person;
+  const doctor = await Doctor.findOne({ email: req.body.email });
+  const patient = await Patient.findOne({ email: req.body.email });
 
   if (!doctor && !patient) return res.status(400).send('Invalid email.');
+
+  let person;
 
   if (doctor) person = doctor;
   if (patient) person = patient;
